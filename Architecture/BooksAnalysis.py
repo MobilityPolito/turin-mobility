80f134880dda10279b8301262d38b0e333c4580d
AleCioc <alessandro@ciociola.me>
2017-01-12 00:25:55 +0100

    added plots for slides

--- Architecture/BooksAnalysis.py
+++ Architecture/BooksAnalysis.py
@@ -1,11 +1,33 @@
+from math import radians, cos, sin, asin, sqrt
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    km = 6367 * c
+    return km
+
 import datetime
 
 import numpy as np
 import pandas as pd
 
+import geopandas as gpd
+from shapely.geometry import Point
+from shapely.geometry import LineString
+
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.style.use('ggplot')
+
+from pandas.tools.plotting import scatter_matrix
 
 from DataBaseProxy import DataBaseProxy
 dbp = DataBaseProxy()
@@ -23,54 +45,37 @@ def get_books_df (provider, city, start, end):
 
     return books_df
     
+provider = "car2go"
+    
 end = datetime.datetime(2016, 12, 10, 0, 0, 0)
 start = end - datetime.timedelta(days = 1)
-books_df = get_books_df("car2go", "torino", start, end)
+books_df = get_books_df(provider, "torino", start, end)
 books_df["durations"] = (books_df["end"] - books_df["start"])/np.timedelta64(1, 'm')
-
-from math import radians, cos, sin, asin, sqrt
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    km = 6367 * c
-    return km
-    
 books_df["distances"] = books_df.apply\
     (lambda row: haversine(row["start_lon"], row["start_lat"], row["end_lon"], row["end_lat"]), axis=1)
-
-books_df = books_df[books_df["durations"] < 200]
-
-from pandas.tools.plotting import scatter_matrix
-scatter_matrix(books_df[["start_lat","start_lon","end_lat","end_lon","durations", "distances"]].astype("float64"), 
-               figsize=(12, 12), diagonal='kde')
-plt.savefig("scatter_matrix.png")
-
-import geopandas as gpd
-from shapely.geometry import Point
-from shapely.geometry import LineString
+books_df = books_df[books_df.durations < 120]
+books_df = books_df[books_df.distances > 1]
 
 books_df["geometry"] = books_df.apply\
     (lambda row: LineString([(row["start_lon"], row["start_lat"]),(row["end_lon"], row["end_lat"])]), axis=1)
 books_df = gpd.GeoDataFrame(books_df, geometry="geometry")
 books_df.crs = {"init": "epsg:4326"}
-
-gdf = gpd.read_file("../../SHAPE/Zonizzazione.dbf")\
+zones = gpd.read_file("../../SHAPE/Zonizzazione.dbf")\
         .to_crs({"init": "epsg:4326"})
-gs = gdf["geometry"]
 
-fig, ax = plt.subplots(1,1,figsize=(20,20))
-ax = gs.plot(color="white", ax=ax)
+#fig, ax = plt.subplots(1,1,figsize=(10,10))
+#ax = scatter_matrix(books_df[["start_lat","start_lon","end_lat","end_lon","durations", "distances"]].astype("float64"), 
+#               figsize=(10, 10), diagonal='kde')
+#plt.savefig(provider + "_books_scatter_matrix.png")
+#
+#zones_geo = zones["geometry"]
+#fig, ax = plt.subplots(1,1,figsize=(10,10))
+#ax = zones_geo.plot(color="white", ax=ax)
+##-car2go
 #ax.set_xlim([7.6, 7.8])
-#ax.set_ylim([45.0,45.15])
-ax = books_df.plot(ax=ax)
-fig.savefig("books.png")
-
+#ax.set_ylim([45.0,45.20])
+##-enjoy
+##ax.set_xlim([7.6, 7.8])
+##ax.set_ylim([44.95,45.12])
+#ax = books_df.plot(ax=ax)
+#fig.savefig(provider + "_books_pattern.png")
--- Architecture/ParksAnalysis.py
+++ Architecture/ParksAnalysis.py
@@ -1,11 +1,19 @@
+from math import radians, cos, sin, asin, sqrt
+
 import datetime
 
 import numpy as np
 import pandas as pd
 
+import geopandas as gpd
+from shapely.geometry import Point
+from shapely.geometry import LineString
+
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.style.use('ggplot')
+
+from pandas.tools.plotting import scatter_matrix
 
 from DataBaseProxy import DataBaseProxy
 dbp = DataBaseProxy()
@@ -23,16 +31,53 @@ def get_parks_df (provider, city, start, end):
 
     return parks_df
     
+provider = "car2go"
+
 end = datetime.datetime(2016, 12, 10, 0, 0, 0)
 start = end - datetime.timedelta(days = 1)
-parks_df = get_parks_df("enjoy", "torino", start, end)
+parks_df = get_parks_df(provider, "torino", start, end)
 parks_df["durations"] = (parks_df["end"] - parks_df["start"])/np.timedelta64(1, 'm')
-parks_df = parks_df[parks_df["durations"] < 200]
 
-from pandas.tools.plotting import scatter_matrix
-scatter_matrix(parks_df[["lat","lon","durations"]].astype("float64"), 
-               figsize=(25, 25), diagonal='kde')
-plt.savefig("scatter_matrix.png")
+zones = gpd.read_file("../../SHAPE/Zonizzazione.dbf")\
+        .to_crs({"init": "epsg:4326"})
+zones["nparks"] = 0.0
+
+parks_df["geometry"] = parks_df.apply\
+    (lambda row: Point(row["lon"], row["lat"]), axis=1)
+parks_df = gpd.GeoDataFrame(parks_df, geometry="geometry")
+parks_df.crs = {"init": "epsg:4326"}
+parks_points = parks_df["geometry"]
+
+for point in parks_points.values:
+    intersect = zones.contains(point)
+    zones.ix[intersect[intersect == True].index.values[0], "nparks"] += 1
+
+#fig, ax = plt.subplots(1,1,figsize=(10,10))
+#ax = parks_df["durations"].hist(figsize=(10,10))
+#plt.savefig(provider + "_parks_durations_hist.png")
+#
+
+#fig, ax = plt.subplots(1,1,figsize=(10,10))
+#ax = scatter_matrix(parks_df[["lat","lon","durations"]].astype("float64"), 
+#               figsize=(10, 10), diagonal='kde')
+#plt.savefig(provider + "_parks_scatter_matrix.png")
+
+#zones_geo = zones["geometry"]
+#fig, ax = plt.subplots(1,1,figsize=(10,10))
+#ax = zones_geo.plot(color="white", ax=ax)
+#ax.set_xlim([7.6, 7.8])
+#ax.set_ylim([45.0,45.15])
+#ax = parks_df.plot(ax=ax)
+#fig.savefig(provider + "_parks_distribution.png")
+
+#import seaborn as sns
+#
+#Index= ['aaa', 'bbb', 'ccc', 'ddd', 'eee']
+#Cols = ['A', 'B', 'C', 'D']
+#df = DataFrame(abs(np.random.randn(5, 4)), index=Index, columns=Cols)
+#
+#sns.heatmap(df)
+
 
 #from pandas.tools.plotting import andrews_curves
 #fig = plt.figure(figsize=(25,25))
